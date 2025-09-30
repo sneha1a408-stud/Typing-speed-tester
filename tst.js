894e7f641a0a941cc22656e5907f9315067c4849
sneha1a408-stud <sneha1a408@gmail.com>
2025-09-30 11:17:56 +0530

    Update tst.js

--- tst.js
+++ tst.js
@@ -1,1 +1,85 @@
+const sentences = [
+"Creativity is intelligence having fun.",
+"Never trust a computer you can’t throw out a window.",
+"Typing is the dance of fingers across a digital stage.",
+"Learning to code is like learning to speak to machines.",
+"Speed is useful only if you're running in the right direction.",
+"A single raindrop can start a ripple across the entire lake.",
+"Books are portals to worlds we’ve never seen but somehow know.",
+"Breakfast tastes better when shared with someone you love.",
+"She found peace in the quiet hum of a sleepy afternoon.",
+"The wind whispered secrets through the rustling leaves."
+  ]
+
+let startTime, selectedSentence;
+let testStarted=False;
+
+function startTest(){
+  selectedSentence=sentences[Math.floor(Math.random() * sentences.length)];
+  document.getElementById("sentence").textContent=selectedSentence;
+  document.getElementById("input").value="";
+  document.getElementById("result").textContent="";
+  startTime=new Date().getTime();
+  testStarted=True;
+}
+
+
+document.getElementById("input").addEventListener("keydown", (e) => {
+  if(!testStarted) return;
+  if(e.key ==="Enter"){
+    e.preventDefault();
+    const typed= document.getElementById("inout").value.trim();
+    const endTime=new Date().getTime();
+    const timeTaken=(endTime-startTime)/1000;
+    const wordCount=selectedSentence.split(" ").length;
+    const wpm=Math.round((wordCount/timeTaken)*60);
+
+    const typedWords=typed.split(" ");
+    let correct=0'
+      for (let i=0; i< originalWords.length; i++) {
+          if(typedWords[i]===originalWords[i]) correct++;
+      }
+
+    const accuracy=Math.round((correct/originalWords.length)*100);
+    const matchMessage=typed===selectedSentence
+      ? "✅ Words match exactly"
+      : "⚠️ Words don't exactly match";
+
+    document.getElementById("result").textContent=
+      '${matchMessage}\n WPM: ${wpm}, Accuracy: ${accuracy}%';
+    testStarted=false;
+  }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
